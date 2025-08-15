56222dc4f8a9268f9e77162eafd7ea3d4dd2d2e9
lescit <lennard.schatz@web.de>
2025-08-15 12:37:45 +0200

    Remove unused config/settings.py from repository

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-class TestConfig:
-    BASE_URL = "https://www.karma.de"
-    DEFAULT_TIMEOUT = 10_000
-    PERFORMANCE_BUDGETS = {
-        "ttfb": 2000,
-        "dcl": 6000,
-        "load": 12000,
-        # optional: "fcp": 3000
-    }
